fd1d12f983cecfbc9ff6ca27ea75dd95f01072cf
Johannah Sims <jjrsims@gmail.com>
2018-01-10 17:16:45 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,4 +6,5 @@ def display_board(board)
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
+  puts ""O"#{board[0]}, "X" #{board[1]}, "X" #{board[5]}, "X" #{board[7]}, "O" #{board[8]}"
 end
